d435a2d97bfcaeab67942ebd77969e520b1d95fc
Foster Stule <camodude5@yahoo.com>
2018-06-04 15:47:04 -0400

    created the class

---
+++ Inheritance/Administrator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+	class Administrator : Employee
+	{
+		public Manager AssignedTo { get; set; }
+
+		public override void Print()
+		{
+			Console.WriteLine($"Administrator: assigned to {AssignedTo.Name}");
+		}
+
+	}
+}
